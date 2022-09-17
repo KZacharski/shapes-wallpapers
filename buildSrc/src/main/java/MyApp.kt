9cc4fb31dba5af92854a85d9a2183a13c53d126c
Kacper Zacharski <puffercatt@gmail.com>
2022-09-17 15:25:09 +0200

    1.0.3

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "xyz.puffercat.shapes.android"
-    const val version = 1
-    const val versionName = "1.0.0"
+    const val version = 4
+    const val versionName = "1.0.3"
 }
